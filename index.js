3abb15ab3ea2fab5fc1893438c49baf7e5fe8a70
toddync <dagvjr@gmail.com>
2024-09-18 18:30:29 -0300

    remove the need for imagemagick

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const {
-    execSync
-} = require('node:child_process');
-const fs = require("node:fs");
+const { exec } = require('node:child_process');
 const CANVAS = require("@napi-rs/canvas");
-const axios = require("axios");
 const dotenv = require('dotenv');
+const axios = require("axios");
+const sharp = require('sharp');
+const fs = require("node:fs");
 
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
-
-const {
-    id,
-    secret,
-    refresh_token
-} = dotenv.config().parsed;
-var token, lastSong;
-
+const { id, secret, refresh_token } = dotenv.config().parsed;
 const screen = {
     width: 1024,
     height: 728
 };
+var token, lastSong;
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 async function start() {
     await refreshToken();
@@ -28,17 +22,16 @@ async function start() {
 
 async function get_img_link() {
     let e = ''
-    
     try {
         e = await axios.get("https://api.spotify.com/v1/me/player/currently-playing", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
+            headers: { Authorization: `Bearer ${token}` }
         });
     } catch (error) {}
 
     if (e != '' && e.status == 200) {
-        show(e.data.item.album.images[1]);
+        try {
+            show(e.data.item.album.images[1]);
+        } catch (error) {}
     }
 }
 
@@ -47,29 +40,24 @@ async function show(img) {
 
     lastSong = img.url;
     let now = Date.now();
-
-    execSync("notify-send 'Changing wallpaper'");
-
     let canvas = CANVAS.createCanvas(screen.width, screen.height);
     let ctx = canvas.getContext('2d');
-
     let image = await CANVAS.loadImage(img.url);
-
     ctx.drawImage(image, 0, 0, screen.width, screen.height);
-    fs.writeFileSync(`images/background_tmp${now}.png`, await canvas.encode("png"));
+    let blurredImage = await CANVAS.loadImage(await sharp(await canvas.encode("png")).blur(10).toBuffer());
 
-    execSync(`magick convert images/background_tmp${now}.png -blur 0x20 images/background_tmp2${now}.png`);
-
-    let _image = await CANVAS.loadImage(`images/background_tmp2${now}.png`);
-    ctx.drawImage(_image, 0, 0, screen.width, screen.height);
-
+    ctx.drawImage(blurredImage, 0, 0, screen.width, screen.height);
     ctx.drawImage(image, screen.width / 2 - img.width / 2, screen.height / 2 - img.height / 2, img.width, img.height);
     fs.writeFileSync(`images/background${now}.png`, await canvas.encode("png"));
 
-    execSync(`feh  --bg-fill images/background${now}.png`);
-    execSync(`wal -i images/background${now}.png`);
-    execSync(`rm images/*${now}.png`);
-    execSync('pywalfox update');
+    try{exec(`wal -i images/background${now}.png`)} catch(e){};
+
+    exec(`gsettings set org.gnome.desktop.background picture-uri file:${__dirname + `/images/background${now}.png`}`);
+    exec(`gsettings set org.gnome.desktop.background picture-uri-dark file:${__dirname + `/images/background${now}.png`}`);
+
+    fs.readdirSync("images/").forEach(f => {
+        if(f != `background${now}.png`) exec(`rm images/${f}`);
+    })
 }
 
 async function refreshToken() {
@@ -83,11 +71,8 @@ async function refreshToken() {
         }
     });
 
-    if (e.status == 200) {
-        token = e.data.access_token;
-    } else {
-        refresh_token();
-    }
+    if (e.status == 200) token = e.data.access_token;
+    else refresh_token();
 }
 
 async function keep() {
